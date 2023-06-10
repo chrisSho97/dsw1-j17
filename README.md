d1a587d3379f713a65ee219993768e79c505f3f2
chrisSho97 <christianpancich_97@hotmail.com>
2023-06-10 16:06:08 -0500

    SERVICIO SOAP

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # curso: Desarrollo de Servicios Web 1 (Grupo V.05.2023-IIE)
+
+## Crear una WS usamdo SOAP
+
---
+++ dsw1-soap-j19/src/main/java/pe/edu/idat/dsw1soapj19/CountryRepository.java
@@ -0,0 +1,61 @@
+package pe.edu.idat.dsw1soapj19;
+
+import jakarta.annotation.PostConstruct;
+import java.util.HashMap;
+import java.util.Map;
+
+import io.spring.guides.gs_producing_web_service.Country;
+import io.spring.guides.gs_producing_web_service.Currency;
+import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
+
+@Component
+public class CountryRepository {
+    private static final Map<String, Country> countries = new HashMap<>();
+
+	@PostConstruct
+	public void initData() {
+		Country spain = new Country();
+		spain.setName("Spain");
+		spain.setCapital("Madrid");
+		spain.setCurrency(Currency.EUR);
+		spain.setPopulation(46704314);
+
+		countries.put(spain.getName(), spain);
+
+		Country poland = new Country();
+		poland.setName("Poland");
+		poland.setCapital("Warsaw");
+		poland.setCurrency(Currency.PLN);
+		poland.setPopulation(38186860);
+
+		countries.put(poland.getName(), poland);
+
+		Country uk = new Country();
+		uk.setName("United Kingdom");
+		uk.setCapital("London");
+		uk.setCurrency(Currency.GBP);
+		uk.setPopulation(63705000);
+
+		countries.put(uk.getName(), uk);
+
+		Country pe = new Country();
+		uk.setName("Peru");
+		uk.setCapital("Lima");
+		uk.setCurrency(Currency.PEN);
+		uk.setPopulation(33000001);
+
+		countries.put(pe.getName(), pe);
+
+
+
+
+	}
+
+	public Country findCountry(String name) {
+		Assert.notNull(name, "The country's name must not be null");
+		return countries.get(name);
+	}
+
+
+}
